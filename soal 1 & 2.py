a0a9b4c74ab47f6bdd5811fcd008861c71f9c9cb
12550111970-abdulaziz <12550111970@students.uin-suska.ac.id>
2026-03-04 09:56:03 +0700

    pbo

---
+++ soal 1 & 2.py
@@ -0,0 +1,38 @@
+class Restaurant:
+    def __init__(self, nama_restoran, jenis_masakan):
+        self.nama_restoran = nama_restoran
+        self.jenis_masakan = jenis_masakan
+
+    def deskripsikan_restoran(self):
+        print("Nama Restoran :", self.nama_restoran)
+        print("Jenis Masakan :", self.jenis_masakan)
+
+    def buka_restoran(self):
+        print(self.nama_restoran, "sekarang buka!\n")
+
+
+restaurant = Restaurant("majestic", "Masakan padang")
+
+
+print("Nama Restoran :", restaurant.nama_restoran)
+print("Jenis Masakan :", restaurant.jenis_masakan)
+print()
+
+
+restaurant.deskripsikan_restoran()
+restaurant.buka_restoran()
+
+
+restaurant1 = Restaurant("majestic", "Masakan Padang")
+restaurant2 = Restaurant("daycino", "Masakan italia")
+restaurant3 = Restaurant("razqa", "Masakan melayu")
+
+
+restaurant1.deskripsikan_restoran()
+print()
+
+restaurant2.deskripsikan_restoran()
+print()
+
+restaurant3.deskripsikan_restoran()
+print()
---
+++ soal 3.py
@@ -0,0 +1,33 @@
+# Membuat class User
+class User:
+    def __init__(self, nama_depan, nama_belakang, umur, email, kota):
+        self.nama_depan = nama_depan
+        self.nama_belakang = nama_belakang
+        self.umur = umur
+        self.email = email
+        self.kota = kota
+
+    def deskripsikan_user(self):
+        print("Nama Lengkap :", self.nama_depan, self.nama_belakang)
+        print("Umur :", self.umur)
+        print("Email :", self.email)
+        print("Kota :", self.kota)
+
+    def sapa_user(self):
+        print("Halo", self.nama_depan + ", selamat datang!\n")
+
+
+# Membuat beberapa objek User
+user1 = User("abdul", "aziz", 19, "abdaziz20@email.com", "Pekanbaru")
+user2 = User("nabila", "amelia", 18, "nbllamelia@email.com", "Medan")
+user3 = User("ahmad", "yoga", 21, "ahmdyoga@email.com", "pasir pengaraian")
+
+# Memanggil method untuk setiap user
+user1.deskripsikan_user()
+user1.sapa_user()
+
+user2.deskripsikan_user()
+user2.sapa_user()
+
+user3.deskripsikan_user()
+user3.sapa_user()
